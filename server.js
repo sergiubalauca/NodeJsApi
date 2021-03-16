322bfbd1bc735d7edbe1b9b1c8028f22edb22c0f
Balauca George-Sergiu <sooty2face@yahoo.com>
2021-03-16 22:48:00 +0200

    another test

--- server.js
+++ server.js
@@ -13,16 +13,15 @@ app.use(cors());
 
 // use JWT auth to secure the api
 app.use(jwt());
-app.use(cors());
+
 // api routes
 app.use('/users', require('./users/users.controller'));
 app.use('/googleTrends', require('./googleTrends/googleTrends.controller'));
-app.get('/:country/removethistext', async (req, res) => {
+app.get('/:country', async (req, res) => {
     try {
         var result = [];
         await googleTrends.dailyTrends({ geo: req.params.country })
             .then(function (results) {
-                console.log('dailyTrends result1: ' + results);
                 var arr = JSON.parse(results).default.trendingSearchesDays[0].trendingSearches
                 for (var i = 0; i < arr.length; i++) {
                     // result.push(arr[i].title.query)
@@ -30,62 +29,10 @@ app.get('/:country/removethistext', async (req, res) => {
                 }
                 res.json(result)
             })
-        console.log('dailyTrends result2: ' + result);
+        console.log(result);
         //res.json({'trend1':trend1,'trend2':trend2})
     } catch (err) { console.log(err) }
 });
-
-// THIS THINGS WORKS, NO GOOGLE REDIRECT...
-app.get("/:country", async (req, res) => {
-    try {
-        //console.log('reached')	        
-        var result = [];
-        var result2 = [];
-        var result3 = [];
-
-        googleTrends.interestOverTime({ keyword: 'test' })
-            .then(function (results) {
-                // console.log((JSON.parse(results).default.timelineData[0]));	
-                JSON.parse(results).default.timelineData.map((data, i) => {
-                    result.push({
-                        'date': data.formattedTime,
-                        'value': data.value[0]
-                    })
-                })
-            }).then(function () {
-                googleTrends.interestOverTime({ keyword: 'test2' }).then(function (results) {	                        // console.log((JSON.parse(results).default.timelineData[0]));	
-                    JSON.parse(results).default.timelineData.map((data, i) => {
-                        result2.push({ 'date': data.formattedTime, 'value': data.value[0] })
-                    })
-                }).then(function () {
-                    googleTrends.interestOverTime({ keyword: 'test' + " " + 'test2' })
-                        .then(function (results) {
-                            // console.log((JSON.parse(results).default.timelineData[0]));	
-                            JSON.parse(results).default.timelineData.map((data, i) => {
-                                result3.push({
-                                    'date': data.formattedTime,
-                                    'value': data.value[0]
-                                })
-                            })
-                            var final = new Array(result.length + 1);
-                            final[0] = new Array(4);
-                            final[0][0] = "Timeline";
-                            final[0][1] = 'test';
-                            final[0][2] = 'test2'
-                            final[0][3] = 'test' + " " + 'test2';
-                            for (var i = 1; i < final.length; i++) {
-                                final[i] = new Array(4);
-                                final[i][0] = result[i - 1] && result[i - 1].date ? result[i - 1].date : "";
-                                final[i][1] = result[i - 1] && result[i - 1].value;
-                                final[i][2] = result2 && result2.length && result2[i - 1].value ? result2[i - 1].value : 0;
-                                final[i][3] = result3 && result3.length && result3[i - 1].value ? result3[i - 1].value : 0;
-                            }
-                            res.json(final);
-                        })
-                })
-            })
-    } catch (err) { console.log(err) }
-})
 
 app.get("/:keyword/:keyword2", async (req, res) => {
     try {
@@ -128,7 +75,7 @@ app.get("/:keyword/:keyword2", async (req, res) => {
                                 final[i] = new Array(4);
                                 final[i][0] = result[i - 1] && result[i - 1].date ? result[i - 1].date : "";
                                 final[i][1] = result[i - 1] && result[i - 1].value;
-                                final[i][2] = result2 && result2.length && result2[i - 1].value ? result2[i - 1].value : 0;
+                                final[i][2] = result2 && result2.length && result2[i - 1].value ? result2[i - 1].value : 0;                            
                                 final[i][3] = result3 && result3.length && result3[i - 1].value ? result3[i - 1].value : 0;
                             }
                             res.json(final);
@@ -165,7 +112,7 @@ app.use(errorHandler);
 
 // start server
 // const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
-const port = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 const server = app.listen(port, function () {
     console.log('Gsb server listening on port ' + port);
