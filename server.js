3eabffc0c0d117846f89df08477a13e4f1a1d0df
Balauca George-Sergiu <sooty2face@yahoo.com>
2021-03-16 18:35:11 +0200

    debugging the daily trends results

--- server.js
+++ server.js
@@ -24,11 +24,12 @@ app.get('/:country', async (req, res) => {
             .then(function (results) {
                 console.log('dailyTrends result1: ' + results);
                 // var arr = JSON.parse(results).default.trendingSearchesDays[0].trendingSearches
-                var arr = JSON.parse(results).default.trendingSearchesDays[0].trendingSearches
-                for (var i = 0; i < arr.length; i++) {
-                    // result.push(arr[i].title.query)
-                    result.push(arr[i])
-                }
+                // var arr = JSON.parse(results).default.trendingSearchesDays[0].trendingSearches
+                // for (var i = 0; i < arr.length; i++) {
+                //     // result.push(arr[i].title.query)
+                //     result.push(arr[i])
+                // }
+                result.push('test');
                 res.json(result)
             })
         console.log('dailyTrends result2: ' + result);
