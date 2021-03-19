84f3413213c14b24e87931c2826f503dc2eb607e
Balauca George-Sergiu <sooty2face@yahoo.com>
2021-03-19 15:21:34 +0200

    trying out cors...

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
