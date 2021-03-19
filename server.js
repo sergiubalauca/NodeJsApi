2762f79a82f9559bc5ef16b80bce39ebf48a0a81
Balauca George-Sergiu <sooty2face@yahoo.com>
2021-03-19 12:57:44 +0200

    test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-// use JWT auth to secure the api
+// use JWT auth to secure the api 
 app.use(jwt());
 
 // api routes 
