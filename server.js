cc1e069241620ef07efe2872e5aaa57a7b6cd5a6
Balauca George-Sergiu <sooty2face@yahoo.com>
2021-03-28 00:51:48 +0200

    brushed up gitignore and add env var mongodb to heroku

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.options('*', cors());
 
 
 const uri = "mongodb+srv://g_sergiu:$erg!u@123@cluster0.ayjsc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Database connected!'));
