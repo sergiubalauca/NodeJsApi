4b8697f2af7f79852725869c04af2f597a0bd510
Balauca George-Sergiu <sooty2face@yahoo.com>
2021-05-05 14:01:49 +0300

    filtered results based on formatted traffic

--- googleTrends/googleTrends.service.js
+++ googleTrends/googleTrends.service.js
@@ -28,26 +28,9 @@ async function getGoogleTrendsFromDB(day, country) {
         if (dailyTrendsDBExists === null) {
             console.log('No daily-trends in db');
         }
-        else {
-            // try {
-            //     // console.log('daily tren exists: ' + dailyTrendsDBExists.length);
+    }).sort({ 'dailyTrends.formattedTraffic': -1 });
 
-            //     for (var i = 0; i < dailyTrendsDBExists.length; i++) {
-            //         console.log('daily trends in service: ' + JSON.stringify(dailyTrendsDBExists[i].dailyTrends.title.query));
-            //         result.push(dailyTrendsDBExists[i].dailyTrends);
-            //     }
-
-            //     // res.json(result);
-            //     // console.log('RAW result: ' + result.length);
-            //     //return json(result);
-            // } catch (error) {
-            //     console.log('THE ERROR: ' + error);
-            // }
-            // console.log('do we have any daily trends? ' + JSON.stringify(result));
-            // console.log('do we have results 1 ? ' + result.length);
-            // return result;
-        }
-    });
+    console.log('Documents found: ' + docs.length);
 
     if (!docs.length == 0) {
         try {
@@ -65,8 +48,7 @@ async function getGoogleTrendsFromDB(day, country) {
             console.log('THE ERROR: ' + error);
         }
     }
-    console.log('do we have results docs ? ' + docs.length);
-    console.log('do we have results 2 ? ' + result.length);
+
     return result;
 }
 
